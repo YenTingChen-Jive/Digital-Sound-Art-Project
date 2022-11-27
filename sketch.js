e64ca254001d3a55c83237fe96f62c64f7e3f70c
yingying0906 <popo090699@gmail.com>
2022-11-27 12:54:30 +0800

    Debug
    - deal with the conflict between wave and star twinkling effect

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
 let table, table_setting;
 var star_array = [];
-var shooting_array = [];
 var img_width, img_height;
 var f;
-var star_length, star_dist, num_shooting;
+var star_length, star_dist;
 var table_r, table_setting_r;
 var star_size_lower, star_size_upper;
 var bgm = [];
@@ -41,9 +40,6 @@ function setup() {
 	// table_iteration
 	table_r = 0;
 	table_setting_r = 0;
-
-	// number of shooting star
-	num_shooting = 7;
 	bgm_now = 0;
 
 	// random center
@@ -52,10 +48,9 @@ function setup() {
 
 	//background
 	createCanvas(windowWidth, windowHeight);
-  background(0);
-  noFill();
-  stroke(255, 45);
-  radius = width;
+	noFill();
+	stroke(255, 45);
+	radius = width;
 }
 
 function draw_star() {
@@ -73,8 +68,8 @@ function draw_star() {
 				star_length = img_height;
 			}
 
-			star_size_upper = star_length * 0.007;
-			star_size_lower = star_length * 0.002;
+			star_size_upper = star_length * 0.015;
+			star_size_lower = star_length * 0.007;
 			star_dist = star_length * 0.00005;
 
 			table_r++;
@@ -95,7 +90,7 @@ function draw_star() {
 		r: int(random(255, 255)),
 		g: int(random(255, 255)),
 		b: int(random(255, 255)),
-		full: int(random(50, 255)),
+		full: 255,
 
 		t: random(TAU),
 		size: star_size,
@@ -130,8 +125,32 @@ function draw_star() {
 			overlapping = true;
 		}
 
+		//delete
+		noStroke();
+		fill(0);
+		quad(
+			other.a_upx,
+			other.a_upy - sin(other.t) * f,
+			other.a_rightx + sin(other.t) * f,
+			other.a_righty,
+			other.a_downx,
+			other.a_downy,
+			other.a_leftx - sin(other.t) * f,
+			other.a_lefty + sin(other.t) * f
+		);
+		quad(
+			other.b_upx,
+			other.b_upy - sin(other.t) * f,
+			other.b_rightx + sin(other.t) * f,
+			other.b_righty,
+			other.b_downx,
+			other.b_downy + sin(other.t) * f,
+			other.b_leftx - sin(other.t) * f,
+			other.b_lefty
+		);
+
 		// redraw stars already exist
-		f = 0.9;
+		f = 1.5;
 		other.t += 0.03;
 		noStroke();
 		fill(255, 255, 255, other.full);
@@ -185,111 +204,44 @@ function draw_star() {
 	}
 }
 
-function draw_shooting_star() {
-	var shooting = {
-		r: 255, // int(random(0, 255))
-		g: 255,
-		b: 255,
-		row: random(0, windowHeight),
-		col: random(0, windowWidth),
-		diameter: int(random(star_length * 0.005, star_length * 0.008)),
-		full: int(random(60, 100)),
-		t: random(TAU),
-		dx: 2,
-		dy: 2,
-	};
-
-	// shooting star number
-	if (shooting_array.length <= num_shooting) {
-		shooting_array.push(shooting);
-		noStroke();
-		fill(shooting.r, shooting.g, shooting.b, shooting.full);
-		ellipse(
-			shooting.col,
-			shooting.row,
-			shooting.diameter,
-			shooting.diameter
-		);
-
-		// traj of shooting stars
-		stroke(255, 255, 255, 10);
-		strokeWeight(3);
-		line(shooting.col, shooting.row, shooting.col - 70, shooting.row - 70);
-	}
-
-	// redraw shooting star
-	for (var j = 0; j < shooting_array.length; j++) {
-		var other = shooting_array[j];
-		other.row += other.dy;
-		other.col += other.dx;
-		if (other.row >= windowHeight - 1 || other.col >= windowWidth - 1) {
-			other.row = random(0, windowHeight / 2);
-			other.col = random(0, windowWidth);
-		}
-
-		// twinkle
-		f = 0.9;
-		other.t += 0.03;
-		noStroke();
-		fill(other.r, other.g, other.b, other.full);
-		var scale = shooting.diameter + sin(shooting.t) * f;
-		ellipse(other.col, other.row, scale, scale);
-
-		// traj
-		stroke(255, 255, 255, 10);
-		strokeWeight(3);
-		line(
-			other.col,
-			other.row,
-			other.col - other.dx * 10,
-			other.row - other.dy * 10
-		);
-	}
-}
-
 radius = 0;
 function draw_background() {
-  var center_x, center_y, noiseFactor, x, y;
-  center_x = mouseX;
-  center_y = mouseY;
-  stroke(100, 54, 60, 30);
-  noFill();
-  beginShape();
-  TOTAL_DEGREES = document.getElementById("degrees").value;
-  
-  for (var i = 0, _pj_a = TOTAL_DEGREES; i < _pj_a; i += 1) {
-    noiseFactor = noise(i * 0.02, Number.parseFloat(frameCount) / 140);
-    x = center_x + radius * cos(radians(i)) * noiseFactor;
-    y = center_y + radius * sin(radians(i)) * noiseFactor;
-    r = random(255);
-    g = random(120);
-    b = random(220);
-    curveVertex(x, y);
-  }
+	var center_x, center_y, noiseFactor, x, y;
+	center_x = mouseX;
+	center_y = mouseY;
+	stroke(100, 54, 60, 30);
+	noFill();
 
-  endShape();
-  radius -= 1;
+	beginShape();
+	TOTAL_DEGREES = document.getElementById("degrees").value;
 
-  if (radius === -600) {
-    radius += 1200;
-  }
+	for (var i = 0, _pj_a = TOTAL_DEGREES; i < _pj_a; i += 1) {
+		noiseFactor = noise(i * 0.02, Number.parseFloat(frameCount) / 140);
+		x = center_x + radius * cos(radians(i)) * noiseFactor;
+		y = center_y + radius * sin(radians(i)) * noiseFactor;
+		r = random(255);
+		g = random(120);
+		b = random(220);
+		curveVertex(x, y);
+	}
+
+	endShape();
+	radius -= 1;
+
+	if (radius === -600) {
+		radius += 1200;
+	}
 }
 
 function draw() {
-	
-  // update background
+	// update background
+
+	draw_background();
 
 	// draw star
 	for (var i = 0; i < 10; i++) {
 		draw_star();
 	}
-
-	// draw shooting star
-	// for (var i = 0; i < 10; i++) {
-	// 	draw_shooting_star();
-	// }
-
-  draw_background();
 }
 
 function keyPressed() {
@@ -309,9 +261,7 @@ function keyPressed() {
 		table = loadTable("csv/" + filename_in + ".csv", "csv");
 
 		//reset
-    background(0);
 		star_array = [];
-		shooting_array = [];
 
 		// new random center
 		ran_center_x = random(5, windowWidth / 3);
@@ -353,9 +303,7 @@ function keyPressed() {
 		table = loadTable("csv/" + filename_in + ".csv", "csv");
 
 		//reset
-    background(0);
 		star_array = [];
-		shooting_array = [];
 
 		// new random center
 		ran_center_x = random(5, windowWidth / 3);
@@ -385,10 +333,10 @@ function keyPressed() {
 function keyReleased() {
 	if (keyCode === RIGHT_ARROW) {
 		table_r = 0;
-    background(0);
+		background(0);
 	} else if (keyCode === LEFT_ARROW) {
 		table_r = 0;
-    background(0);
+		background(0);
 	}
 }
 
